549927cbc2b76ff3ffa29f536c54d94d43835c07
GK <jpkutner+github@gmail.com>
2021-04-18 11:22:16 -0500

    made markers

--- woodland/views.py
+++ woodland/views.py
@@ -18,7 +18,21 @@ def list_trees(request):
 
 def map_trees(request):
     context = {
-        "google_maps_api_key": os.getenv("GOOGLE_MAPS_API_KEY")
+        "google_maps_api_key": os.getenv("GOOGLE_MAPS_API_KEY"),
+        "trees": [
+            {
+                "lat": 34.5,
+                "lng": -86.4,
+                "url": "/",
+                "name": "Fabulous Cheese Tree"
+            },
+            {
+                "lat": 34.4,
+                "lng": -86.5,
+                "url": "/",
+                "name": "Fabulous Money Tree"
+            }
+        ]
     }
     return render(request, "map.html", context)
 
